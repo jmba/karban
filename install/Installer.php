1451cc12a38fae89f5896c762a515d15f950411e
Matthias Endler <matthias-endler@gmx.net>
2010-07-12 15:06:13 +0200

    Creating installer class to encapsulate setup process.

---
+++ install/Installer.php
@@ -0,0 +1,173 @@
+<?php
+
+/**
+ * This installer class collects some lightweight 
+ * special functions needed for karban setup.
+ */
+
+require_once("../settings.php"); 		// Load often used settings and objects
+require_once(LIB_PATH . "Tables.php");	// Manage database tables
+require_once(LIB_PATH . "Message.php");	// Format warnings and erros
+
+class Installer {
+	
+	private $db;
+	private $dbHost;
+	private $dbName;
+	private $dbUser;
+	private $dbPass;
+	
+	function Installer($dbHost = DB_HOST, $dbName = DB_NAME, 
+		$dbUser = DB_USER, $dbPass = DB_PASS) 
+	{
+		$this->setConnection($dbHost, $dbName, $dbUser, $dbPass);
+	}
+
+	function setConnection($dbHost, $dbName, $dbUser, $dbPass) {
+		$this->dbHost = sanitize($dbHost);
+		$this->dbName = sanitize($dbName);
+		$this->dbUser = sanitize($dbUser);
+		$this->dbPass = sanitize($dbPass);
+		echo $this->dbHost;
+	}
+	
+	/**
+	 * Check database connection. 
+	 * Returns true if connection established.
+	 */ 
+	function hasConnection() {
+		try {
+			$this->db = new PDO("mysql:host=" . $this->dbHost . ";dbname=" . $this->dbName, 
+				$this->dbUser, $this->dbPass);
+			echo new Message("Connected to database.", "Well done!", MessageType::SUCCESS);
+			return true;
+		} catch(PDOException $e) {
+			echo new Message("Could not connect to database.",
+				"This is normal for the first start. 
+				Please configure karban below <br />(for advanced options modify 
+				<em>settings.php</em> and reload this page).", 
+			MessageType::INFO, $e->getMessage(), MoreTextType::DEBUG);
+			return false;
+		}
+	}
+	
+	/* 
+	 * Find out if karban is already installed.
+	 * Returns true if already installed.
+	 */
+	function isInstalled() {
+		
+		// First load karban table names (with prefix).
+		$setupTables = new Tables(DB_PREFIX);
+		$tableNames = $setupTables->getTables();
+		
+		// Then load tables from database
+		if (!$db) return; // No database object.
+		$results = $this->db->query("SHOW TABLES");
+		$installedTables = $results->fetchAll(PDO::FETCH_COLUMN);
+		
+		// If we find a table in database that belongs to karban
+		// we asume that karban is already installed.
+		foreach ($installedTables as $table) {
+			if ( in_array($table, $tableNames)) {
+				echo new Message("Karban is already installed.",
+				"You have three possibilites:<br />
+				1. If you want to install another copy of karban adjust the <em>settings.php</em> file
+				in the root directory (set new prefix or database name).<br />
+				2. If you want reinstall karban remove the tables in the <em>". DB_NAME ."</em> database<br />
+				3. Remove this <em>install</em> directory to prevent accidential reinstall.",
+				 MessageType::WARNING,
+				"The table <strong>". $table . "</strong> was found. It belongs to a karban installation. 
+				A previous installation of karban is present.", MoreTextType::DEBUG);
+				return true;
+			}
+		}
+		return false; // Karban is not yet installed.
+	}
+	
+	/**
+	 * Show a setup form for karban installation
+	 */
+	function showSetupForm() {
+		?>
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactform">
+	
+					    <label id="l_dbname" title="The name of the database karban will be installed into." for="dbname">Database name:</label>
+						<input type="text" size="30" name="dbname" id="dbname" value="<?php echo DB_NAME ?>" />
+	
+	
+					<div>
+						<label id="l_dbuser" title="Username to access the database." for="dbuser">Database username:</label>
+						<input type="text" size="30" name="dbuser" id="dbuser" value="<?php echo DB_USER ?>" />
+					</div>
+	
+					<div>
+						<label id="l_dbpass1" title="Password to access the database." for="dbpass1">Database password:</label>
+						<input type="password" size="30" name="dbpass1" id="dbpass1" value="<?php echo DB_PASS ?>" />
+					</div>
+					
+					<div>
+						<label id="l_dbpass2" title="Check for typing mistake in password." for="dbpass2">Repeat password:</label>
+						<input type="password" size="30" name="dbpass2" id="dbpass2" value="<?php echo DB_PASS ?>" />
+					</div>
+	
+				    <input type="submit" value="Connect" class="button" name="submit" />
+				    
+				    <!--  Tooltips -->
+				   	<script type='text/javascript'> 
+					  	$(function() {
+					    	$('#dbname').tipsy({fade: true, gravity: 's'});
+					    	$('#dbuser').tipsy({fade: true, gravity: 's'});
+					    	$('#dbpass1').tipsy({fade: true, gravity: 's'});
+					    	$('#dbpass2').tipsy({fade: true, gravity: 's'});
+	  					});
+					</script> 
+				</form>
+		<?php	
+	}	
+	
+	function install() {
+		$debugMsg = "Installation started...<br />";
+		if($this->db->query($setupTables->getTable("Groups"))) {
+			$debugMsg .=  DB_PREFIX ."Groups table created...<br />";
+		} else {
+			$debugMsg .=  "Error constructing ". DB_PREFIX ."Groups table.<br />";
+			$db_issue = true;
+		}
+	
+		if($this->db->query($setupTables->init("Groups"))) {
+			$debugMsg .=  "Inserted Standard User group into ". DB_PREFIX ."Groups table...<br />";
+		} else {
+			$debugMsg .=  "Error constructing Groups table.<br />";
+			$db_issue = true;
+		}
+	
+		if($this->db->query($setupTables->getTable("Users"))) {
+			$debugMsg .=  DB_PREFIX. "Users table created.....<br />";
+		} else {
+			$debugMsg .=  "Error constructing user table.<br />";
+			$db_issue = true;
+		}
+	
+		if(!$db_issue) {
+			echo new Message("Karban has been installed", 
+				"Please remove this <em>install</em> folder for security reasons.", 
+				MessageType::SUCCESS);
+		} else {
+			echo new Message("Oops! Something went wrong with the database setup.", 
+				"Please check your configuration and <a href=\"?install=true\">start again</a>", 
+				MessageType::ERROR, $debugMsg . "<br />" . $db->_sql_error(), MoreTextType::DEBUG);
+		}
+	}
+	
+	/*
+	 * Helper method returning a user input 
+	 * string without special characters. 
+	 * @param $str
+	 */
+	function sanitize($str) {
+		return strip_tags(trim(($str)));
+	}
+}
+
+?>
--- install/index.php
+++ install/index.php
@@ -7,96 +7,44 @@
  *  _|  _|    _|    _|  _|    _|  _|    _|  _|    _|  _|    _|_|
  *  _|    _|  _|    _|  _|    _|  _|_|_|    _|    _|  _|      _|
  *
- * This script installs KARBAN on your server (via bootstrap) using settings.php from the
- * root directory. Delete the install folder after the installation is complete.
+ * This script installs karban on your server (via bootstrap)
+ * using settings.php from the root directory. 
+ * Delete the install folder after the installation is complete.
  */
 
-include_once("header.html");		// Print a basic html header
-require_once("../settings.php"); 	// Load often used settings and objects
+require_once("Installer.php"); 			// Class handling installation routine
+require_once(LIB_PATH .	"Button.php");	// CSS Buttons
 
+include_once("header.html");			// Print a basic html header
+$installer = new Installer();
+start($installer);
 
-// Check database connection
-try {
-	$db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-	echo new Message("Connected to database.", "Well done!", MessageType::SUCCESS);
-} catch(PDOException $e) {
-	echo new Message("Could not connect to database.",
-	"This is normal for the first start. 
-	Please configure karban below <br />(for advanced options modify 
-	<em>settings.php</em> and reload this page).", 
-	MessageType::INFO, $e->getMessage(), MoreTextType::DEBUG);
-	require_once(LIB_PATH . "helpers.php");
-	showSetupForm();
-	exit;
-}
-
-// Find out if karban is already installed:
-
-// First load karban table names (without prefix).
-include_once( LIB_PATH . "Tables.php");
-$setupTables = new Tables(DB_PREFIX);
-$tableNames = $setupTables->getTables();
-// Then load tables from database
-$results = $db->query("SHOW TABLES");
-$installedTables = $results->fetchAll(PDO::FETCH_COLUMN);
-
-// If we find a table in database that belongs to karban
-// we asume that karban is already installed.
-foreach ($installedTables as $table) {
-	if ( in_array($table, $tableNames)) {
-		echo new Message("Karban is already installed.",
-		"You have three possibilites:<br />
-		1. If you want to install another copy of karban adjust the <em>settings.php</em> file
-		in the root directory (set new prefix or database name).<br />
-		2. If you want reinstall karban remove the tables in the <em>". DB_NAME ."</em> database<br />
-		3. Remove this <em>install</em> directory to prevent accidential reinstall.",
-		 MessageType::WARNING,
-		"The table <strong>". $table . "</strong> was found. It belongs to a karban installation. 
-		A previous installation of karban is present.", MoreTextType::DEBUG);
-		exit;
+// Installation routine
+function start($installer) {
+	if ($installer->hasConnection()) {
+		if ($installer->isInstalled()) {
+			// Already installed. No work for installer
+		} else {
+			// We are connected. Offer installation.
+			echo "<br />You are one step away from a simple project management 
+				system that is <em>fun</em> to use.<br /><br />";
+			echo new Button("Install", "?install=true", ButtonType::POSITIVE);
+		}		
+	} else {
+		// No connection to database. Ask for credentials.
+		$installer->showSetupForm();	
 	}
 }
 
-// Karban is not yet installed. Offer installation:
+// Read user credentials and start installation
 if(isset($_GET["install"])) {
-	$debugMsg = "Installation started...<br />";
-	if($db->query($setupTables->getTable("Groups"))) {
-		$debugMsg .=  DB_PREFIX ."Groups table created...<br />";
-	} else {
-		$debugMsg .=  "Error constructing ". DB_PREFIX ."Groups table.<br />";
-		$db_issue = true;
-	}
-
-	if($db->query($setupTables->init("Groups"))) {
-		$debugMsg .=  "Inserted Standard User group into ". DB_PREFIX ."Groups table...<br />";
-	} else {
-		$debugMsg .=  "Error constructing Groups table.<br />";
-		$db_issue = true;
-	}
-
-	if($db->query($setupTables->getTable("Users"))) {
-		$debugMsg .=  DB_PREFIX. "Users table created.....<br />";
-	} else {
-		$debugMsg .=  "Error constructing user table.<br />";
-		$db_issue = true;
-	}
-
-	if(!$db_issue) {
-		echo new Message("Karban has been installed", 
-			"Please remove this <em>install</em> folder for security reasons.", 
-			MessageType::SUCCESS);
-	} else {
-		echo new Message("Sorry. Something went wrong with the database setup.", 
-			"Please check your configuration and <a href=\"?install=true\">start again</a>", 
-			MessageType::ERROR, $debugMsg . "<br />" . $db->_sql_error(), MoreTextType::DEBUG);
-	}
-} else {
-	include_once(LIB_PATH .	"Button.php");
-	?>
-		<br />You are one step away from a simple project management system that is <em>fun</em> to use.<br /><br />
-	<?php
-	echo new Button("Install", "?install=true", ButtonType::POSITIVE);
+	$installer->install();
+	exit;
+} elseif ($_POST) {
+	$installer->setConnection(DB_HOST ,$_POST["dbname"],$_POST["dbuser"],$_POST["dbpass"]);
+	start($installer);
 }
 
 include_once("footer.html");
+
 ?>
